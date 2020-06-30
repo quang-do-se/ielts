4690b4497a9f5023ef5a2af725283c0a35fb3c49
Quang Do <quang.do.se@gmail.com>
2020-06-30 15:17:00 -0600

    update

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ var pitchValue = document.querySelector('.pitch-value');
 var rate = document.querySelector('#rate');
 var rateValue = document.querySelector('.rate-value');
 
+var newButton = document.querySelector('#new-button');
+var repeatButton = document.querySelector('#repeat-button');
+
+var showResultButton = document.querySelector('#show-result-button');
+var resultTxt = document.querySelector('#result-txt');
+
+var guessButton = document.querySelector('#guess-button');
+var guessTxt = document.querySelector('#guess-txt');
+var guessResult = document.querySelector('#guess-result');
+
 var voices = [];
 
 function populateVoiceList() {
@@ -24,20 +34,19 @@ function populateVoiceList() {
 
 populateVoiceList();
 
+// Weird loading issue
 if (speechSynthesis.onvoiceschanged !== undefined) {
   speechSynthesis.onvoiceschanged = populateVoiceList;
 }
 
-function speak(){
+function speak(phrases){
   if (synth.speaking) {
     console.error('speechSynthesis.speaking');
     return;
   }
 
-  for (let i = 0; i < 10; i++) {
-    phrase = dict[Math.floor(Math.random() * dict.length)];
-
-    var utterThis = new SpeechSynthesisUtterance(phrase);
+  for (let i = 0; i < phrases.length; i++) {
+    var utterThis = new SpeechSynthesisUtterance(phrases[i]);
 
     utterThis.onend = function (event) {
       console.log('SpeechSynthesisUtterance.onend');
@@ -49,8 +58,8 @@ function speak(){
 
     utterThis.voice = voices[Math.floor(Math.random() * voices.length)];
 
-    console.log("Phrase: '" + phrase + "'. Using voice: " + utterThis.voice.name + ", " + utterThis.voice.lang);
-    
+    console.log("Phrase: '" + phrases[i] + "'. Using voice: " + utterThis.voice.name + ", " + utterThis.voice.lang);
+
     utterThis.pitch = pitch.value;
     utterThis.rate = rate.value;
 
@@ -66,8 +75,54 @@ rate.onchange = function() {
   rateValue.textContent = rate.value;
 }
 
-inputForm.onsubmit = function(event) {
+var phrases = [];
+var result = '';
+
+newButton.onclick = function(event) {
+  event.preventDefault();
+  phrases = [];
+  result = '';
+  
+  for (let i = 0; i < 3; i++) {
+    phrases[i] = dict[Math.floor(Math.random() * dict.length)];
+    result += phrases[i] + "\n";
+  }
+
+  speak(phrases);
+  
+  // trim last line break
+  result = result.replace(/\n$/, "").toLowerCase();
+}
+
+repeatButton.onclick = function(event) {
   event.preventDefault();
 
-  speak();
+  speak(phrases);
+}
+
+guessButton.onclick = function(event) {
+  event.preventDefault();
+
+  if (guessTxt.value.toLowerCase() === result) {
+    guessResult.innerText = 'Correct!';
+  } else {
+    guessResult.innerText = 'Wrong!';
+  }
+}
+
+
+var showResult = false;
+
+showResultButton.onclick = function(event) {
+  event.preventDefault();
+
+  if (showResult) {
+    resultTxt.innerHTML = '';
+    showResultButton.innerText = 'Show result';
+  } else {
+    resultTxt.innerHTML = result;
+    showResultButton.innerText = 'Hide result';
+  }
+
+  showResult = !showResult;
 }
